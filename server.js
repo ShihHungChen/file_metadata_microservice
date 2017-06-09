cffc53d5e85901d932ceee183adb261b92fdf9c5
Shi Hung <c.shihhung@gmail.com>
2017-06-09 15:51:44 +0000

    Initial static html file

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const express = require('express')
-const app = express()
-
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+var express = require('express')
+var app = express()
+var multer  = require('multer')
+var upload = multer()
+ 
+app.post('/profile', upload.single(), function (req, res, next) {
+  res.end();
 })
-
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!')
 })

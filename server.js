3e7ed02ede0af3e9afab613e0dd3a62dff4334e5
Shi Hung <c.shihhung@gmail.com>
2017-06-10 03:17:33 +0000

    server load html file

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var app = express()
 var multer  = require('multer')
 var upload = multer()
  
+app.use(express.static(__dirname + '/Public')); // Note: the path should totally correct, 'Public' != 'public'
+ 
 app.post('/profile', upload.single(), function (req, res, next) {
   res.end();
 })

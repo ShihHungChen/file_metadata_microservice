e586f8d1f96b2b7850b2585db3322c71f9359043
Shi Hung <c.shihhung@gmail.com>
2017-06-11 16:10:34 +0000

    Complete post response

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-var express = require('express')
-var app = express()
-var multer  = require('multer')
-var upload = multer()
- 
+var express = require('express');
+var multer  = require('multer');
+var upload = multer();
+
+var app = express();
+
 app.use(express.static(__dirname + '/Public')); // Note: the path should totally correct, 'Public' != 'public'
  
-app.post('/profile', upload.single(), function (req, res, next) {
-  res.end();
-})
+app.post('/upload', upload.single('fileupload'), function(req, res){
+  res.json({size : req.file.size});
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!')
 })
